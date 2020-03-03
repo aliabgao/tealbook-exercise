2387865e2757fa77b2a704b92d165a791e75d097
= <ali.abgao@hotmail.com>
2020-03-03 02:07:46 -0500

    remove node modules and console logs.

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ async function getClimateData(date){
     
 async function callGetClimateData(date){
     const climateData = await getClimateData(date);
-    console.log(climateData);
 }
 
 // call to function
